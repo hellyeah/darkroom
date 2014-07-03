0d4cd1522eec8c66ecd23600ad006375016b1299
ricky <ricky.medina91@gmail.com>
2014-07-03 08:38:07 -0700

    demo

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
-var nav,
-    carousel
+var nav
 
 $(document).ready(function() {
   nav = new Nav()
   nav.init()
 
-  carousel = new Carousel()
+  $('.carousel').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1230,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 })
--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 var Nav = function() {
   this.$window = $(window)
   this.$sections = $('.scroll')
+  this.navOriginalTop = $('.navbar').offset().top
 
   $('[class*=nav-scroll]').each(function(ind, el) {
     var classes =  el.className.split(/\s+/)
@@ -16,6 +17,7 @@ var Nav = function() {
     }
   })
 }
+
 
 Nav.prototype._activeNav = function() {
   var self = this,
@@ -34,10 +36,22 @@ Nav.prototype._activeNav = function() {
   })
 }
 
+
+Nav.prototype._navHeight = function() {
+  var top = window.scrollY
+
+  if (top > this.navOriginalTop)
+    $('.navbar').css('position', 'fixed').css('top', 0)
+  else
+    $('.navbar').css('position', 'absolute').css('top', this.navOriginalTop)
+}
+
+
 Nav.prototype.init = function() {
   var self = this
 
   this.$window.scroll(function(e) {
     self._activeNav()
+    self._navHeight()
   })
 }
